ce357a6666209423c168c503c8487ed23f3a72cf
jrojas96 <jeffreyrm96@gmail.com>
2017-09-24 19:39:01 -0500

    Project Created... Main Tabbed Activity + Gym Company Login Fonts and colors defined

---
+++ FitGym/app/src/main/java/com/teamgym/fitgym/models/GymCompany.java
@@ -0,0 +1,14 @@
+package com.teamgym.fitgym.models;
+
+/**
+ * Created by Usuario on 24/09/2017.
+ */
+
+public class GymCompany {
+    private int gymCompanyId;
+    private String gymCompanyName;
+    private String username;
+    private String password;
+    private String telephone;
+
+}
---
+++ FitGym/app/src/main/java/com/teamgym/fitgym/models/SuscriptionType.java
@@ -0,0 +1,9 @@
+package com.teamgym.fitgym.models;
+
+/**
+ * Created by Usuario on 24/09/2017.
+ */
+
+public class SuscriptionType {
+
+}
