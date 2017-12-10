dae96b2ee6cd019863e5440f68dc1989484291c8
jrojas96 <jeffreyrm96@gmail.com>
2017-12-09 23:52:28 -0500

    Account fragment deleted

--- FitGym/app/src/main/java/com/teamgym/fitgym/activities/gymcompany/NavigationGymCompanyActivity.java
+++ FitGym/app/src/main/java/com/teamgym/fitgym/activities/gymcompany/NavigationGymCompanyActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import com.teamgym.fitgym.R;
-import com.teamgym.fitgym.fragments.gymcompany.AccountFragment;
 import com.teamgym.fitgym.fragments.gymcompany.CompanyFragment;
 import com.teamgym.fitgym.fragments.gymcompany.PersonalFragment;
 import com.teamgym.fitgym.fragments.gymcompany.SettingsFragment;
